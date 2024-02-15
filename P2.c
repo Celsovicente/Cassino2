f518af8d3c49eb1aa6750184596f4897abc32fd3
Celsovicente <celsovicente222@gmail.com>
2024-02-15 18:46:23 +0100

    Parte da geracao dos numeros

--- P2.c
+++ P2.c
@@ -1,101 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ERRO 1.0e-7
 
-// declarações de funções 
-int modulo(int n);
+//declaração das funções
+int lerSemente();
 int fatorial(int n);
-int potencia(int base, int expoente);
+float modulo(float x);
+float arredondar(float z);
 float seno();
-float arrendodar();
-int acertarNaoCapicua(int numSorteado, int numApostado);
-int acertarCapicua(int numSorteado, int numApostado);
-int acertarDezena(int numSorteado, int numApostado);
-int acertarUnidade(int numSorteado, int numApostado);
-int acertarUnidadeDezena(int numSorteado, int numApostado);
-int ordemInversaPar(int numSorteado, int numApostado);
-int ordemInversaImpar(int numSorteado, int numApostado);
-int somaDosDigitos(int numSorteado, int numApostado);
-float calcularLucro();
+float converterDecimal(float x);
+float potencia(int base, int expoente);
+int inverso(int n);
+int capicua(int n);
+int somaDigitos(int n);
 
-// funcionamento das funções
-int modulo(int n)
+
+// funcionamento das funcoes para gerar numero
+int lerSemente()
 {
-	int aux;
-		if( n < 0)
-			aux = n * -1;
-		else
-			aux = n;
-	return aux;
+	int n, flag = 0;
+	while(flag == 0)
+	{
+		printf("\n Digite um numero:\n");
+		scanf("%d",&n);
+		if(n <= 0)
+			printf("\n Erro, valor invalido!");
+		else if(n > 9999999)
+			printf("\n Erro, valor invalido!");
+			else
+			flag = 1;
+	}
+	return n;
+}
+
+float modulo(float x)
+{
+	if(x < 0.0)
+	{
+		return x = -x;
+	}
+	return x;
 }
 
 int fatorial(int n)
 {
-	int fat = 1, i; 
-	if(n == 0)
-		return fat;
-		else
-		{
-			for(i = 1; i <= n; i++)
-			fat*=i;
-		}
-	return fat;		
+	int fat = 1, i;
+	for(i = 1; i<= n; i++)
+	fat*=i;
+	return fat;
 }
 
-int potencia(int base, int expoente)
+float potencia(int base, int expoente)
 {
-	int pot = 1, i;
+	float pot = 1.0;
+	int i;
 	if(expoente == 0)
 		pot = 1;
+	else if( expoente > 0)
+	{
+		for(i = 1; i<= expoente; i++)
+		{
+			pot*= base;
+		}
+	}
 	else
 	{
 		for(i = 1; i <= expoente; i++)
-		{
-			pot*=base;
-		}
+		pot = 1 / (pot * base);
 	}
 	return pot;
 }
 
-int acertarNaoCapicua(int numApostado, int numSorteado)
+
+float arredondar(float z)
 {
-	
+	float decimal;
+	int inteiro;
+	inteiro = z;
+	if(z < 0)
+	{
+		decimal = z - (int)z;
+		if(decimal != 0)
+		{
+			if(inteiro > 0)
+			{
+				inteiro++;
+			}
+			else
+			{
+				inteiro--;				
+			}
+		}
+	}
+	return inteiro;
 }
 
-int acertarCapicua(int numApostado, int numSorteado)
+float converterDecimal(float x)
 {
 	
+    float i = 10.0;
+    while (x > 1.0) {
+        x = x / i;
+    }
+    return x;
 }
 
-int acertarDezena(int numApostado, int numSorteado)
+
+// funcoes para as regras
+int capicua(int n)
 {
-	
+	int unidade, dezena, a;
+	unidade = n % 10;
+	dezena = n / 10;
+	if(dezena == unidade)
+	{
+		a = 1;
+		return a;
+	}
+	else
+	{
+		a = 0;
+		return a;
+	}
 }
 
-int acertarUnidade(int numApostado, int numSorteado)
+int inverso(int n)
 {
-	
+	int inverso;
+	inverso = ((n % 10) * 10) + n / 10;
+	return inverso;
 }
 
-int acertarUnidadeDezena(int numApostado, int numSorteado)
+int somaDigitos(int n)
 {
-	
-}
-
-int ordemInversaPar(int numApostado, int numSorteado)
-{
-	
-}
-
-int ordemInversaImpar(int numApostado, int numSorteado)
-{
-	
-}
-
-int somaDosDigitos(int numApostado, int numSorteado)
-{
-	float lucro;
-	if(numApostado == ((numSorteado / 10) + (numSorteado % 10)))	
-	
+	int soma, unidade, dezena;
+	unidade = n % 10;
+	dezena = n / 10;
+	soma = unidade + dezena;
+	return soma;	
 }
 
 int main(int argc, char *argv[]) 
