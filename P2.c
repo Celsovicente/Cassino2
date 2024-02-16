3ae32732b62400d794b352224ccc875a50b56b53
Celsovicente <celsovicente222@gmail.com>
2024-02-16 20:17:09 +0100

    Gerando numeros

--- P2.c
+++ P2.c
@@ -3,17 +3,20 @@
 #define ERRO 1.0e-7
 
 //declaração das funções
+int gerarNumero();
 int lerSemente();
 int fatorial(int n);
 float modulo(float x);
 float arredondar(float z);
-float seno();
+float seno(float x);
 float converterDecimal(float x);
 float potencia(int base, int expoente);
 int inverso(int n);
 int capicua(int n);
 int somaDigitos(int n);
-
+int parOuImpar(int n);
+float calcularLucro(float lucro, float prejuizo);
+void unir();
 
 // funcionamento das funcoes para gerar numero
 int lerSemente()
@@ -105,6 +108,47 @@ float converterDecimal(float x)
     return x;
 }
 
+float seno(float x)
+{
+    float atual = 0.0 , anterior, seno;
+    int i = 1, j = 1, flag = 1;
+ 
+	if(x != 0)
+	{
+	    while(flag == 1)
+	    {
+		    
+			if(i%2 == 0)
+			{
+			    atual -=  potencia(x,(j + 2)) / fatorial((j + 2));
+   			    if(modulo(atual - anterior) < ERRO)
+			   {
+					 flag = 0;
+		       	}
+			}
+			else
+			{
+		  	    anterior +=  potencia(x, (j + 2))/ fatorial((j + 2));
+			} 
+			i++;
+		}
+	}
+	
+return atual;
+}
+
+// funcao para gerar numero
+int gerarNumero()
+{
+	int x, n;
+	float y, Y, z;
+	x = lerSemente();
+	y = converterDecimal(x);
+	z = 9821.0 * modulo(seno(y) + 0.211327);
+	Y = z - arredondar(z);
+	n = (int)(y* 50 + 1);
+	return n;
+}
 
 // funcoes para as regras
 int capicua(int n)
@@ -138,6 +182,46 @@ int somaDigitos(int n)
 	dezena = n / 10;
 	soma = unidade + dezena;
 	return soma;	
+}
+
+int parOuImpar(int n)
+{
+	int a;
+	if(n % 2 == 0)
+	{
+		a = 1;
+		return a;
+	}
+	else
+	{
+		a = 0;
+		return a;
+	}
+	
+}
+
+float calcularLucro(float lucro, float prejuizo)
+{
+	if(lucro > prejuizo)
+	{
+		lucro-= prejuizo;
+		return lucro;
+	}
+	else if(lucro == prejuizo)
+	{
+		lucro = prejuizo = 0;
+		return lucro;
+	}
+	else
+	{
+		prejuizo = prejuizo - lucro;
+		return prejuizo;
+	}
+}
+
+void unir()
+{
+	
 }
 
 int main(int argc, char *argv[]) 
